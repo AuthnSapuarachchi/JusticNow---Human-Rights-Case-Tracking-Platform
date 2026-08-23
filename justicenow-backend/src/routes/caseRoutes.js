27fc59f0c8c93f6133a349a5fcccc9eca2ecd13c
AuthnSaparachchi <authn.sapuarachchi@gmail.com>
2026-08-24 00:19:05 +0530

    middlewarea had issue fixed

--- justicenow-backend/src/routes/caseRoutes.js
+++ justicenow-backend/src/routes/caseRoutes.js
@@ -6,7 +6,7 @@ const { submitCase, trackCase } = require('../controllers/caseController');
 // const { getOfficerCases } = require('../controllers/officerController'); // Example for your teammates
 
 // 2. Import Middleware
-const { protectRoute, authorizeRoles } = require('../middleware/authMiddleware');
+const { protectRoute, authorizeRoles } = require('../middlewares/authMiddleware');
 
 
 // 🌍 PUBLIC CITIZEN ROUTES (No Token Needed)
