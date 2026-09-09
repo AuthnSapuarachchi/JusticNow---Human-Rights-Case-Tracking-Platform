c5fdd85d13e469d0a697af47272aae07578ca76d
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-09-09 11:55:54 +0530

    feat: initialize server with express and set up basic routes

---
+++ justicenow-backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const caseRoutes = require('./routes/caseRoutes');
+const authRoutes = require('./routes/authRoutes');
+const officerRoutes = require('./routes/officerRoutes');
+
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); // Parses incoming JSON payloads
+app.use(morgan('dev')); // Logs API requests to the console
+
+// Basic Health Check Route (For Sprint 1 Demo)
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'success', message: 'JusticeNow API is running' });
+});
+
+// We will mount our modular routes here later
+app.use('/api/auth', authRoutes);
+// c
+app.use('/api/cases', caseRoutes);
+
+app.use('/api/officer', officerRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
